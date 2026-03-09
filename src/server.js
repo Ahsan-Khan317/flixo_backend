98e58e36dc3ac85e22cc38c16aedf44f3f9c9c9f
md . ahsan khan <sk6582757@gmail.com>
2026-03-09 16:54:03 +0530

    updated origin

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://flixo12.netlify.app/',
+  origin: 'https://flixo12.netlify.app',
   credentials: true
 }));
 app.use(express.json());
